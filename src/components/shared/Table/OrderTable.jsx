c9a2b512a5179e5f87b2b7ee9696a273862fa7af
alaz <akaleli1@binghamton.edu>
2022-07-08 23:38:56 -0400

    Added JSON to Orders.

--- src/components/shared/Table/OrderTable.jsx
+++ src/components/shared/Table/OrderTable.jsx
@@ -1,9 +1,56 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from '@fortawesome/free-solid-svg-icons'
 
-
+const orders = [ 
+  {
+    id:1,
+    date:'20 Jan 2022',
+    subtotal:'75.32',
+    total:'82.1',
+    item_title:'Red Fjallraven Kanken Backpack',
+    first_name:'Alaz',
+    last_name:'Kalelioglu',
+  },
+  {
+    id:2,
+    date:'20 Jan 2022',
+    subtotal:'63.32',
+    total:'67.12',
+    item_title:'Fjallraven - Foldsack No. 1 Backpack',
+    first_name:'Kevin',
+    last_name:'Hart',
+  },
+  {
+    id:3,
+    date:'21 Jan 2022',
+    subtotal:'103.32',
+    total:'111.4',
+    item_title:'Mens Casual Slim Fit',
+    first_name:'Nick',
+    last_name:'Carter',
+  },
+  {
+    id:4,
+    date:'22 Jan 2022',
+    subtotal:'234.53',
+    total:'243.59',
+    item_title:'White Gold Plated Princess',
+    first_name:'Steve',
+    last_name:'Rogers',
+  },
+  {
+    id:5,
+    date:'25 Jan 2022',
+    subtotal:'21.99',
+    total:'28.67',
+    item_title:'WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive',
+    first_name:'Mikasa',
+    last_name:'Hopkins',
+  },
+]
 
 function Order() {
+
  
 
   return (
